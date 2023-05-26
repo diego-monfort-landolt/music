9fd2cf858220ce64c4a1e1df0f230dc7e821f5c8
Diego19890102 <diegomonfort1989@gmail.com>
2023-05-26 21:25:00 +0200

    Update playlist.js

--- js/playlist.js
+++ js/playlist.js
@@ -3,7 +3,7 @@ let songs = [
         name : "Test Musik Player",
         artist : "Diego Monfort Landolt",
         img : "img1",
-        audio : "./music/music1.mp3"
+        audio : "music1.mp3"
     },
     {
         name : "2 Song",
